67e7ae491c3658d609737032eefe9d5e40a485b6
Iryna-L <47776176+Iryna-L@users.noreply.github.com>
2019-11-29 10:47:55 +0200

    fixed version

--- findSubstring.js
+++ findSubstring.js
@@ -1,15 +1,16 @@
 const inputSearch = document.getElementById("wordSearch")
 
-const allText = document.getElementById('main').innerHTML
+const allHtml = document.getElementById('main').innerHTML
+const allText = document.getElementById('main').innerText
 
 inputSearch.onkeyup = findWord;
 
 function findWord(e) {
-  if (allText.indexOf(e.target.value)) {
-    let newText = allText.split(e.target.value).join(`<span class="found_word">${e.target.value}</span>`)
+  if (allText.indexOf(e.target.value) >= 0) {
+    let newText = allHtml.split(e.target.value).join(`<span class="found_word">${e.target.value}</span>`)
     document.getElementById('main').innerHTML = newText
         
   } else {
-    document.getElementById('main').innerHTML = allText
+    document.getElementById('main').innerHTML = allHtml
   }
 }
