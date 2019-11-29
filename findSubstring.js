cf87cf10e25614ceee012a421eea47a3265531ea
Iryna-L <47776176+Iryna-L@users.noreply.github.com>
2019-11-29 15:29:18 +0200

    correct working script with html tags

--- findSubstring.js
+++ findSubstring.js
@@ -3,13 +3,36 @@ const inputSearch = document.getElementById("wordSearch")
 const allHtml = document.getElementById('main').innerHTML
 const allText = document.getElementById('main').innerText
 
+let indexStart = -1;
+let indexEnd = -1;
+let tag = ''
+let temporaryReplacer = ''
+let tags = []
+const space = "~"
+
+while ((indexStart = allHtml.indexOf('<', indexStart + 1)) != -1) {
+  indexEnd = allHtml.indexOf('>', indexEnd + 1)
+  tag = allHtml.slice(indexStart, indexEnd+1)
+  temporaryReplacer = space.repeat(indexEnd - indexStart + 1)
+  tags.push({ tag, indexStart, indexEnd, temporaryReplacer })
+}
+
+let modifiedHtml = allHtml
+
+let newString= " "
+tags.forEach(el => {
+  modifiedHtml = modifiedHtml.replace(el.tag, el.temporaryReplacer)
+})
+
 inputSearch.onkeyup = findWord;
 
 function findWord(e) {
-  if (allText.indexOf(e.target.value) >= 0) {
-    let newText = allHtml.split(e.target.value).join(`<span class="found_word">${e.target.value}</span>`)
+  if (allText.indexOf(e.target.value) >= 0 && inputSearch.value) {
+    let newText = modifiedHtml.split(e.target.value).join(`<span class="found_word">${e.target.value}</span>`)
+    tags.forEach(el => {
+      newText = newText.replace(el.temporaryReplacer, el.tag)
+    })
     document.getElementById('main').innerHTML = newText
-        
   } else {
     document.getElementById('main').innerHTML = allHtml
   }
